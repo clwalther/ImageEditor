340a89114c795c475cc108a0efe61041ae36df2c
clwalther <c.l.walther@outlook.de>
2023-02-02 21:40:42 +0100

    refactoring and quick update on rotation

--- __main__.py
+++ __main__.py
@@ -7,35 +7,36 @@ class PNGBild():
         self.height = None
         self.width = None
 
-    def laden(self, name: str) -> bool:
-        ## lädt eine Datei und belegt die Attribute pixeldata, height und width
-        ## Hier bitte nichts ändern!
+    def load(self, name: str) -> int:
+        """Loads data from file into list format.
+
+            Returns: 0 if image was successfully loaded.
+            """
         img = QtGui.QImage()
         if img.load(name):
             img = img.convertToFormat(QtGui.QImage.Format_RGB888)
             self.height = img.height()
             self.width = img.width()
-            rohdaten = list(img.scanLine(0).asarray(img.sizeInBytes()))
+            bareData = list(img.scanLine(0).asarray(img.sizeInBytes()))
             self.pixeldata = []
-            for i in range(0, len(rohdaten), 3):
-                self.pixeldata.append(rohdaten[i:i+3])
-            return True
-        else:
-            print("Ein-/Ausgabefehler beim Laden der Datei '" + name + "'.! Ist die Datei lesbar, im richtigen Format und der Name richtig?")
-            return False
+            for i in range(0, len(bareData), 3):
+                self.pixeldata.append(bareData[i:i+3])
+            return 0
+        return -1
 
-    def speichern(self, name: str) -> bool:
-        ## speichert das Bild in einer Datei
-        ## Hier bitte nichts ändern!
+    def save(self, name: str) -> int:
+        """Writes the data into a file and saves it.
+
+            Returns: 0 if saving was successful.
+        """
         if self.pixeldata and self.height and self.width:
-            flache_liste = []
+            flatList = []
             for pixel in self.pixeldata:
-                flache_liste += pixel
-            img = QtGui.QImage(bytes(flache_liste), self.width, self.height, self.width*3, QtGui.QImage.Format_RGB888)
+                flatList += pixel
+            img = QtGui.QImage(bytes(flatList), self.width, self.height, self.width*3, QtGui.QImage.Format_RGB888)
             if img.save(name):
-                print("Datei erfolgreich gespeichert")
-            else:
-                print("Ein-/Ausgabefehler beim Speichern der Datei '" + name + "'.! Ist die Datei bzw. das Verzeichnis schreibbar?")
+                return 0
+        return -1
 
     def getPixel(self, x: int, y: int) -> list:
         """Returns the pixel value of a given pixel-x-y."""
@@ -49,18 +50,18 @@ class PNGBild():
         self.pixeldata[y * self.width + x] = value
         return 0
 
+
 class BilderApp(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        ## Lädt die Benutzeroberfläche und regstriert unsere Zeichenmethode
         super().__init__(parent)
         uic.loadUi('.index.ui', self)
         self.canvas.paintEvent = self.draw
 
         self.image = None
 
-        ## verknüpft die Buttons mit den entsprechenden Methoden
-        self.button_oeffnen.clicked.connect(self.datei_oeffnen)
-        self.button_speichern.clicked.connect(self.datei_speichern)
+        ## connect buttons to methods
+        self.button_oeffnen.clicked.connect(self.openFile)
+        self.button_speichern.clicked.connect(self.saveFile)
         self.button_drehen_links.clicked.connect(self.rotateClkWise)
         self.button_drehen_rechts.clicked.connect(self.rotateCrClkWise)
         # self.button_spiegeln_horizontal.clicked.connect(...)
@@ -69,26 +70,35 @@ class BilderApp(QtWidgets.QMainWindow):
         # self.button_invertieren.clicked.connect(...)
         self.slider_helligkeit.valueChanged.connect(self.changedBrightness)
 
-    def datei_oeffnen(self):
-        ## Öffnet einen Datei-Dialog und lädt die gewählte Datei in self.image
-        ## Hier bitte nichts ändern!
-        options = QtWidgets.QFileDialog.Options()
-        datei_name, _ = QtWidgets.QFileDialog.getOpenFileName(self,
-            "Bilddatei öffnen", "","PNG-Bilder (*.png);;Alle Dateien (*)", options=options)
-        if datei_name:
-            self.image = PNGBild()
-            self.image.laden(datei_name)
-        self.repaint() # löst den paintEvent aus, damit die neue Datei gleich angezeigt wird.
+    def openFile(self) -> int:
+        """Opens file explorer instance and loads a chosen PNG File into the 'PNGBild' class.
 
-    def datei_speichern(self):
-        ## Öffnet einen Datei-Dialog und speichert self.image in der gewählten Datei
-        ## Hier bitte nichts ändern!
+            Returns:
+                int: Result of 'PNGBild' method 'load' or -1 if there is no file.
+            """
+        options = QtWidgets.QFileDialog.Options()
+        name, _ = QtWidgets.QFileDialog.getOpenFileName(self,
+            "Open Files", "","PNG-Files (*.png);;All Files (*)", options=options)
+        if name:
+            self.image = PNGBild()
+            loaded = self.image.load(name)
+            self.update()
+            return loaded
+        return -1
+
+    def saveFile(self) -> int:
+        """Opens file explorer insance and saves a the current image into a PNG File.
+
+            Returns:
+                int: Result of 'PNGBild' method 'save' or -1 if there is no instance of 'PNGBild'.
+                """
         if self.image:
             options = QtWidgets.QFileDialog.Options()
-            datei_name, _ = QtWidgets.QFileDialog.getSaveFileName(self,
-                "Datei speichern","","PNG-Bilder (*.png);;Alle Dateien (*)", options=options)
-            if datei_name:
-                self.image.speichern(datei_name)
+            name, _ = QtWidgets.QFileDialog.getSaveFileName(self,
+                "Save Files","","PNG-Files (*.png);;All Files (*)", options=options)
+            if name:
+                return self.image.save(name)
+        return -1
 
     def draw(self, event: QtCore.QEvent):
         """Draws the current image to the canvas.
@@ -129,17 +139,17 @@ class BilderApp(QtWidgets.QMainWindow):
         """Rotate the image clockwise by 90°.
 
             Returns:
-                int: Result of calling the `rotate` function with turnIndex = 1.
+                int: Result of calling the `rotate` function with turnIndex = 3.
             """
-        return self.rotate(1)
+        return self.rotate(3)
 
     def rotateCrClkWise(self) -> int:
         """Rotate the image counter-clockwise by 90°.
 
             Returns:
-                int: Result of calling the `rotate` function with turnIndex = 3.
+                int: Result of calling the `rotate` function with turnIndex = 1.
             """
-        return self.rotate(3)
+        return self.rotate(1)
 
     def rotate(self, turnIndex: int) -> int:
         """Rotate the image by 90°, 180°, or 270° clockwise.
@@ -161,19 +171,19 @@ class BilderApp(QtWidgets.QMainWindow):
             flatList = []
 
             imageHeight = self.image.height
-            imageWdith  = self.image.width
+            imageWidth  = self.image.width
 
-            self.image.height = imageWdith
+            self.image.height = imageWidth
             self.image.width  = imageHeight
 
             for y in range(imageHeight):
-                start = imageWdith * (y + 0)
-                stop  = imageWdith * (y + 1)
+                start = imageWidth * (y + 0)
+                stop  = imageWidth * (y + 1)
                 flatList.append(self.image.pixeldata[start:stop])
 
             for x in range(imageHeight):
-                for y in range(imageWdith):
-                    self.image.setPixel(x, y, flatList[x][y])
+                for y in range(imageWidth):
+                    self.image.setPixel(imageHeight - x - 1, y, flatList[x][y])
 
         self.update()
 
@@ -188,9 +198,6 @@ class BilderApp(QtWidgets.QMainWindow):
             return -1
 
         return 0
-
-
-
 
 
 def main():
